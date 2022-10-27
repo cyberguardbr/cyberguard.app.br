4e1405e4b8cb1d17dbaa22ed2b56df048a3d95b4
cyberguard541 <103520872+cyberguard541@users.noreply.github.com>
2022-10-26 23:55:06 -0300

    Update ubuntu_openconnect.sh

--- ubuntu_openconnect.sh
+++ ubuntu_openconnect.sh
@@ -2,9 +2,9 @@
 cp /usr/share/zoneinfo/Asia/Dubai /etc/localtime
 
 #Database Details
-db_host='193.123.124.209';
+db_host='99.79.66.228';
 $db_user = "cyberlink";
-$db_pass = "Cyberlink3005";
+$db_pass = "Cyberlink3005!";
 $db_name = "cyberlink";
 
 install_require()
