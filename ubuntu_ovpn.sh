28a4afe12fde29779018b95c4f24fd3529307430
cyberguard541 <103520872+cyberguard541@users.noreply.github.com>
2022-10-30 14:20:35 -0300

    Update ubuntu_ovpn.sh

--- ubuntu_ovpn.sh
+++ ubuntu_ovpn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-$db_host='99.79.38.168';
-$db_user = "cyberlink";
+$db_host='35.183.135.53';
+$db_user = "root";
 $db_pass = "Cyberlink3005!";
 $db_name = "cyberlink";
 install_require()
