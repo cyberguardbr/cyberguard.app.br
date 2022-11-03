2d9567bed7e0972c65a524374dd896c2ab7e503d
cyberguard541 <103520872+cyberguard541@users.noreply.github.com>
2022-11-03 17:58:54 -0300

    Update ubuntu_openconnectws.sh

--- ubuntu_openconnectws.sh
+++ ubuntu_openconnectws.sh
@@ -2,8 +2,8 @@
 cp /usr/share/zoneinfo/Asia/Dubai /etc/localtime
 
 #Database Details
-$db_host='35.183.135.53';
-$db_user = "root";
+$db_host='193.123.117.248';
+$db_user = "cyberlink";
 $db_pass = "Cyberlink3005!";
 $db_name = "cyberlink";
 
